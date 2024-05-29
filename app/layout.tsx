0d7af48a137ed96b627880a7570704f2a1cef423
Spencer Evans <spencerevans234@gmail.com>
2024-05-28 22:41:11 -0400

    Initial Commit set up initial file structure

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "NewFilm",
+  description: "Movies, TV Shows, and more all in one place. NewFilm is the new home for media on the web.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
