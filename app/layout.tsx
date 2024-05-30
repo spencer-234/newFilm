6f01884b1a39c61b92ff5a301e2f75ddb1494ad3
Spencer Evans <spencerevans234@gmail.com>
2024-05-30 07:12:33 -0400

    complete initial navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "NewFilm",
   description: "Movies, TV Shows, and more all in one place. NewFilm is the new home for media on the web.",
+  icons: {
+    icon: 'assets/logo.png',
+  }
 };
 
 export default function RootLayout({
@@ -17,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <main>
           {children}
         </main>
